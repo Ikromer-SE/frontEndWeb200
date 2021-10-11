8e00970c821368ff88c48e0f289f3d9296c80d15
Ikromer-SE <73547137+Ikromer-SE@users.noreply.github.com>
2021-10-11 15:25:09 -0400

    Refactor to use createSelectors

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,4 +1,4 @@
-import { ActionReducerMap } from '@ngrx/store'
+import { ActionReducerMap, createSelector } from '@ngrx/store'
 import * as fromCounter from './counter.reducer'
 
 export interface AppState {
@@ -9,10 +9,15 @@ export const reducers: ActionReducerMap<AppState> = {
   counter: fromCounter.reducer
 }
 
-export function selectCounterCurrent(state: AppState) {
-  return state.counter.current;
-}
+const selectCounterBranch = (state: AppState) => state.counter;
 
-export function selectResetDisabled(state: AppState) {
-  return state.counter.current === 0;
-}
+export const selectCounterCurrent = createSelector(
+  selectCounterBranch,
+  b => b.current
+)
+
+export const selectResetDisabled = createSelector(
+  selectCounterCurrent,
+  c => c === 0
+)
+

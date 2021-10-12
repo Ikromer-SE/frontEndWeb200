56af5e13d156bb40fe42fddb194425ac0ffbbed1
Ikromer-SE <73547137+Ikromer-SE@users.noreply.github.com>
2021-10-12 12:03:27 -0400

    Save counter goal to local storage

--- src/app/effects/settings.effects.ts
+++ src/app/effects/settings.effects.ts
@@ -1,16 +1,25 @@
 import { Injectable } from "@angular/core";
-import { Actions, createEffect } from "@ngrx/effects";
-import { tap } from 'rxjs/operators';
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { map, tap } from 'rxjs/operators';
+import * as actions from '../actions/settings.actions'
 
 @Injectable()
 export class SettingsEffects {
 
-  logThemAll$ = createEffect(() =>
+  // logThemAll$ = createEffect(() =>
+  //   this.actions$.pipe(
+  //     tap(a => console.log(`Got an actions of type ${a.type}`))
+  //   )
+  //   , { dispatch: false }
+  // );
+
+  saveCountGoal$ = createEffect(() =>
     this.actions$.pipe(
-      tap(a => console.log(`Got an actions of type ${a.type}`))
-    )
-    , { dispatch: false }
-  );
+      ofType(actions.countGoalChanged), // only care this one... -> countGoalChanged Action
+      map(a => a.newGoal), // countGoalChanges -> number
+      map(a => a.toString()), // number => string
+      tap(goal => localStorage.setItem('counterGoal', goal))
+    ), { dispatch: false })
 
   constructor(private actions$: Actions) { }
 }

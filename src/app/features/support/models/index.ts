ced03a4fd4e14569d57224b9c94f2c24c625222f
Ikromer-SE <73547137+Ikromer-SE@users.noreply.github.com>
2021-10-12 16:31:39 -0400

    Update UI for support component

---
+++ src/app/features/support/models/index.ts
@@ -0,0 +1,10 @@
+export interface SupportModel {
+  supportPhone: string | null;
+  status: string;
+  currentlyOpen: boolean;
+  supportContact: {
+    name: string;
+    emailAddress: string;
+    phone: string | null;
+  }
+}
--- src/app/features/support/support.component.ts
+++ src/app/features/support/support.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SupportModel } from './models';
 
 @Component({
   selector: 'app-support',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./support.component.css']
 })
 export class SupportComponent implements OnInit {
+
+  words = ['dog', 'cat']
+  model: SupportModel = {
+    supportContact: {
+      name: 'Sue',
+      emailAddress: 'sue@aol.com',
+      phone: '555-1212'
+    },
+    supportPhone: null,
+    status: 'Groovy',
+    currentlyOpen: true
+  }
 
   constructor() { }
 
